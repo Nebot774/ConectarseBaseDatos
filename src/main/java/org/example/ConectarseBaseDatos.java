c2aefc6f17ec77ee929032982611c142c26617de
Nebot774 <adriclemenbot@gmail.com>
2023-11-22 16:25:32 +0100

    Commit:Comentarios añadidos

--- src/main/java/org/example/ConectarseBaseDatos.java
+++ src/main/java/org/example/ConectarseBaseDatos.java
@@ -8,28 +8,30 @@ import java.time.LocalDate;
 
 public class ConectarseBaseDatos {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://accessodatosdb.cpfwt1hqt9da.us-east-1.rds.amazonaws.com:5432/f12006";
-        String user = "postgres";
-        String password = "Secreto!2023";
+        // Definición de las credenciales de la base de datos
+        String url = "..."; // URL de conexión a la base de datos
+        String user = "postgres"; // Usuario de la base de datos
+        String password = "Secreto!2023"; // Contraseña de la base de datos
 
         Connection conn = null;
         try {
+            // Establecer la conexión con la base de datos
             conn = DriverManager.getConnection(url, user, password);
+            // Desactivar auto-commit para manejar transacciones manualmente
             conn.setAutoCommit(false);
             System.out.println("Conectado a la base de datos");
 
-            String sqlInsertarConstructor = "INSERT INTO constructors (constructorid, constructorref, name, nationality, url) "
-                    + "VALUES (?, ?, ?, ?, ?) ON CONFLICT (constructorid) DO NOTHING RETURNING constructorid;";
+            // Preparar la sentencia SQL para insertar en la tabla constructors
+            String sqlInsertarConstructor = "...";
             int constructorId = -1;
             try (PreparedStatement pstmt = conn.prepareStatement(sqlInsertarConstructor, Statement.RETURN_GENERATED_KEYS)) {
-                pstmt.setInt(1, 15);
-                pstmt.setString(2, "seat");
-                pstmt.setString(3, "Seat F1");
-                pstmt.setString(4, "España");
-                pstmt.setString(5, "http://seatf1.example.com");
+                // Establecer los valores de los parámetros en la sentencia
+                pstmt.setInt(1, 15); // ... otros pstmt.setString/setInt ...
 
+                // Ejecutar la sentencia y obtener el número de filas afectadas
                 int filasAfectadas = pstmt.executeUpdate();
                 if (filasAfectadas > 0) {
+                    // Recuperar las claves generadas (ID del constructor)
                     try (ResultSet rs = pstmt.getGeneratedKeys()) {
                         if (rs.next()) {
                             constructorId = rs.getInt(1);
@@ -41,47 +43,35 @@ public class ConectarseBaseDatos {
                 }
             }
 
+            // Continuar con la inserción en la tabla drivers si la inserción anterior fue exitosa
             if (constructorId != -1) {
-                String sqlInsertDriver = "INSERT INTO drivers (driverid, code, forename, surname, dob, nationality, constructorid, url) "
-                        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+                // Preparar la sentencia SQL para insertar en la tabla drivers
+                String sqlInsertDriver = "...";
                 try (PreparedStatement pstmtDriver = conn.prepareStatement(sqlInsertDriver)) {
-                    // Carlos Sainz
-                    pstmtDriver.setInt(1, 39);
-                    pstmtDriver.setString(2, "CSAI");
-                    pstmtDriver.setString(3, "Carlos");
-                    pstmtDriver.setString(4, "Sainz");
-                    LocalDate fechaSainz = LocalDate.parse("1994-09-01");
-                    pstmtDriver.setDate(5, Date.valueOf(fechaSainz)); // Convertir la fecha de String a java.sql.Date
-                    pstmtDriver.setString(6, "Spanish");
-                    pstmtDriver.setInt(7, constructorId);
-                    pstmtDriver.setString(8, "http://carlossainz.example.com");
+                    // Insertar el primer piloto
+                    pstmtDriver.setInt(1, 39); // ... otros pstmt.setString/setInt ...
                     pstmtDriver.executeUpdate();
                     System.out.println("Carlos Sainz insertado con éxito.");
 
-                    // Manuel Aloma
-                    pstmtDriver.setInt(1, 40);
-                    pstmtDriver.setString(2, "ALM");
-                    pstmtDriver.setString(3, "Manuel");
-                    pstmtDriver.setString(4, "Aloma");
-                    LocalDate fechaAloma = LocalDate.parse("1990-01-01");
-                    pstmtDriver.setDate(5, Date.valueOf(fechaAloma)); // Convertir la fecha de String a java.sql.Date
-                    pstmtDriver.setString(6, "Spanish");
-                    pstmtDriver.setInt(7, constructorId);
-                    pstmtDriver.setString(8, "http://manuelaloma.example.com");
+                    // Insertar el segundo piloto
+                    pstmtDriver.setInt(1, 40); // ... otros pstmt.setString/setInt ...
                     pstmtDriver.executeUpdate();
                     System.out.println("Manuel Aloma insertado con éxito.");
 
+                    // Confirmar todas las operaciones realizadas en la transacción
                     conn.commit();
                 } catch (SQLException e) {
+                    // Manejar errores al insertar pilotos y hacer rollback de la transacción
                     System.out.println("Error al insertar pilotos: " + e.getMessage());
                     conn.rollback();
-                    // No lanzamos la excepción aquí para permitir que se cierre la conexión
                 }
             }
         } catch (SQLException e) {
+            // Manejar errores generales de conexión o de operaciones en la base de datos
             System.out.println("Error al conectar a la base de datos o al realizar operaciones: " + e.getMessage());
             e.printStackTrace();
         } finally {
+            // Asegurarse de cerrar la conexión a la base de datos
             if (conn != null) {
                 try {
                     conn.close();
@@ -92,4 +82,5 @@ public class ConectarseBaseDatos {
         }
     }
 }
+
 
